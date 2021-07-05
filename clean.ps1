81428208de6b6c7cb729470ca7b9c6e4e2c4f6b0
AndreasGottardi <andreas.gottardi@gmx.at>
2021-07-05 20:01:13 +0200

    Subdirectory added, cleanup improved.

--- clean.ps1
+++ clean.ps1
@@ -2,6 +2,6 @@
     This script removes all generated files and resets the certification authoroty.
 #>
 
-Remove-Item -Path @("*.crt", "*.csr", "*.pem", "*.key", "ca\*.old", "ca\*.attr", "ca\certs\*.pem")
+Remove-Item -Path @("*.crt", "*.csr", "*.pem", "*.key", "ca\*.old", "ca\*.attr", "ca\certs\*.pem", "ca\rootca.crt", "ca\rootca.key", "certs") -ErrorAction SilentlyContinue -Recurse
 Set-Content -Path "ca\ca.db.index" -Value "" -NoNewline
 Set-Content -Path "ca\ca.db.serial" -Value "01"
--- server.ps1
+++ server.ps1
@@ -20,49 +20,52 @@ param (
 	$DnsAltIPs = @("127.0.0.1", "::1")
 )
 
-if(-Not (Test-Path "${ServerName}")){
+$CertDir = "certs"
+
+if(-Not (Test-Path "${CertDir}\${ServerName}")){
 	
-	New-Item -ItemType "Directory" -Path "${ServerName}"
-	Set-Content -Path "${ServerName}\${ServerName}.cnf" -Value "[req]"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "distinguished_name = req_distinguished_name"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "req_extensions = v3_req"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "prompt = no"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "[req_distinguished_name]"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "C = AT"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "ST = Vorarlberg"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "L = Dornbirn"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "O = Goa Systems"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "OU = IT department"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "CN = ${ServerName}"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "emailAddress = andreas.gottardi@goa.systems"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "[v3_req]"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "keyUsage = keyEncipherment, dataEncipherment"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "extendedKeyUsage = serverAuth"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "subjectAltName = @alt_names"
-	Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "[alt_names]"
+	New-Item -ItemType "Directory" -Path "${CertDir}\${ServerName}"
+	Set-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "[req]"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "distinguished_name = req_distinguished_name"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "req_extensions = v3_req"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "prompt = no"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "[req_distinguished_name]"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "C = AT"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "ST = Vorarlberg"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "L = Dornbirn"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "O = Goa Systems"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "OU = IT department"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "CN = ${ServerName}"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "emailAddress = andreas.gottardi@goa.systems"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "[v3_req]"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "keyUsage = keyEncipherment, dataEncipherment"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "extendedKeyUsage = serverAuth"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "subjectAltName = @alt_names"
+	Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "[alt_names]"
 
 	for ($i = 0; $i -lt $DnsAltNames.Count; $i++) {
-		Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "DNS.$($i + 1) = $($DnsAltNames[$i])"
+		Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "DNS.$($i + 1) = $($DnsAltNames[$i])"
 	}
 
 	for ($i = 0; $i -lt $DnsAltIPs.Count; $i++) {
-		Add-Content -Path "${ServerName}\${ServerName}.cnf" -Value "IP.$($i + 1)  = $($DnsAltIPs[$i])"
+		Add-Content -Path "${CertDir}\${ServerName}\${ServerName}.cnf" -Value "IP.$($i + 1)  = $($DnsAltIPs[$i])"
 	}
-	
+
+	<#
+		The following two lines create a certificate signing request.
+	#>
+	& $OpenSSL genrsa -out "${CertDir}\${ServerName}\${ServerName}.key" 4096
+	& $OpenSSL req -new -out "${CertDir}\${ServerName}\${ServerName}.csr" -key "${CertDir}\${ServerName}\${ServerName}.key" -config "${CertDir}\${ServerName}\${ServerName}.cnf"
+
+	<#
+		The following line signs the csr.
+	#>
+	& $OpenSSL ca -batch -config .\ca\rootca.cnf -out "${CertDir}\${ServerName}\${ServerName}.crt" -infiles "${CertDir}\${ServerName}\${ServerName}.csr"
+
+	<#
+		The following command generates a pfx file from the private key and certificate.
+	#>
+	& $OpenSSL pkcs12 -export -out "${CertDir}\${ServerName}\${ServerName}.pfx" -inkey "${CertDir}\${ServerName}\${ServerName}.key" -in "${CertDir}\${ServerName}\${ServerName}.crt" -password pass:$ServerName -name "$ServerName"
+} else {
+	Write-Host -Object "Certificate already exists."
 }
-
-<#
-	The following two lines create a certificate signing request.
-#>
-& $OpenSSL genrsa -out "${ServerName}\${ServerName}.key" 4096
-& $OpenSSL req -new -out "${ServerName}\${ServerName}.csr" -key "${ServerName}\${ServerName}.key" -config "${ServerName}\${ServerName}.cnf"
-
-<#
-	The following line signs the csr.
-#>
-& $OpenSSL ca -batch -config .\ca\rootca.cnf -out "${ServerName}\${ServerName}.crt" -infiles "${ServerName}\${ServerName}.csr"
-
-<#
-	The following command generates a pfx file from the private key and certificate.
-#>
-& $OpenSSL pkcs12 -export -out "${ServerName}\${ServerName}.pfx" -inkey "${ServerName}\${ServerName}.key" -in "${ServerName}\${ServerName}.crt" -password pass:$ServerName
